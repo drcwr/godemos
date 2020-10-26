6eb7f8c6f2522fcbf7a53a52e92801ca421e3ca4
Wurui Chen <chenwurui@163.com>
2020-10-26 19:51:52 +0800

    bibao-arg

---
+++ waitgroup.go
@@ -0,0 +1,38 @@
+package main
+
+// 闭包不加接收的参数，就会默认使用外部变量的地址拷贝
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	input()
+	noinput()
+}
+
+func input() {
+	wg := sync.WaitGroup{}
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		// time.Sleep(time.Duration(1) * time.Microsecond)
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
+
+func noinput() {
+	wg := sync.WaitGroup{}
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		// time.Sleep(time.Duration(1) * time.Microsecond)
+		go func() {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
