745fac1b59d3bba5f4acf71c94fcceb1051bc9e6
Wurui Chen <chenwurui@163.com>
2021-01-08 15:38:35 +0800

    map init

---
+++ map-init.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+)
+
+func initLog() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+type mapstr map[string]string
+
+func main() {
+	initLog()
+	var maptestvar map[string]string
+	log.Printf("maptestvar %#v\n", maptestvar)
+	if maptestvar == nil {
+		// maptestvar = make(map[string]string)
+		maptestvar = mapstr{}
+	}
+
+	maptestvar["testnil"] = "nilvalue"
+
+	maptest := make(map[string]chan struct{})
+	log.Printf("maptest make %#v\n", maptest)
+
+	maptest["del"] = make(chan struct{}, 1)
+
+	log.Printf("maptest %#v\n", maptest)
+	log.Printf("maptest 1 %#v\n", maptest["del"])
+
+}
