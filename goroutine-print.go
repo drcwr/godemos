67dc60890cf880ffb73e5106fd76c22baa49bc27
Wurui Chen <chenwurui@163.com>
2021-01-26 14:13:16 +0800

    go for Printer

--- goroutine-print.go
+++ goroutine-print.go
@@ -6,26 +6,28 @@ import (
 	"time"
 )
 
+func Printer(ch chan int, wg *sync.WaitGroup) {
+	select {
+	case num, ok := <-ch:
+		if ok {
+			fmt.Printf("Printer%d-%d\n", num%3+1, num+1)
+			num++
+			ch <- num
+			wg.Done()
+		}
+	}
+}
+
 func main() {
 	var (
 		ch = make(chan int, 100)
 		wg = sync.WaitGroup{}
 	)
 	defer close(ch)
-
 	wg.Add(100)
+
 	for i := 0; i < 100; i++ {
-		go func() {
-			select {
-			case num, ok := <-ch:
-				if ok {
-					fmt.Printf("Printer%d-%d\n", num%3+1, num+1)
-					num++
-					ch <- num
-					wg.Done()
-				}
-			}
-		}()
+		go Printer(ch, &wg)
 	}
 
 	ch <- 0
