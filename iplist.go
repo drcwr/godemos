fb98a1f80a667c8462dcef7d60cdceed2dff7521
Wurui Chen <chenwurui@163.com>
2021-01-27 18:01:15 +0800

    for arr

--- iplist.go
+++ iplist.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var t = make([][][4]int, 9)
@@ -14,22 +17,45 @@ func main() {
 			}
 		}
 	}
-	// for i := 0; i < len(t); i++ {
-	// fmt.Printf("%d,len=%d\nt:%v\n\n", i+4, len(t[i]), t[i])
-	// }
 
-	var input = "123212122"
+	var input = "12561211"
 	var bytea = []byte(input)
+	var ipstr = [4][]byte{}
 	var i = len(input)
+labeltotal:
 	for k, v := range t[i-4] {
 		fmt.Printf("%d-%d,%d,%d,%d----", k, v[0], v[1], v[2], v[3])
+		ipstr[0] = bytea[0:v[0]]
+		ipstr[1] = bytea[v[0] : v[0]+v[1]]
+		ipstr[2] = bytea[v[0]+v[1] : v[0]+v[1]+v[2]]
+		ipstr[3] = bytea[v[0]+v[1]+v[2]:]
+		for i := 0; i < 4; i++ {
+			if !checkipnum(ipstr[i]) {
+				continue labeltotal
+			}
 
-		fmt.Printf("%v.", string(bytea[0:v[0]]))
-		fmt.Printf("%v.", string(bytea[v[0]:v[0]+v[1]]))
-		fmt.Printf("%v.", string(bytea[v[0]+v[1]:v[0]+v[1]+v[2]]))
-		fmt.Printf("%s", string(bytea[v[0]+v[1]+v[2]:]))
+		}
+		fmt.Printf("%v", string(ipstr[0]))
+		for i := 1; i < 4; i++ {
+			fmt.Printf(".%v", string(ipstr[i]))
+		}
+
 		fmt.Printf("\n")
 
 	}
 
+}
+
+func checkipnum(input []byte) bool {
+	numip, err := strconv.Atoi(string(input))
+	if err != nil {
+		fmt.Printf("CHECK FALSE,%s\n", err.Error())
+		return false
+	}
+	if numip > 255 || numip < 0 {
+		fmt.Printf("CHECK FALSE,numip %v\n", numip)
+		return false
+	}
+
+	return true
 }
