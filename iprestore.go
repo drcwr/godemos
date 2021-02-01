8e46540cc4c5e2d05a8ceefe5d6d3ca1b8cf31ae
Wurui Chen <chenwurui@163.com>
2021-02-01 17:28:22 +0800

    ip restroe dfs

--- iprestore.go
+++ iprestore.go
@@ -3,7 +3,45 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+func main() {
+	list()
+	dfsmain()
+}
+
+func dfs(s string, begin int, path []string, res *[]string) {
+
+	if len(path) > 4 {
+		return
+	}
+
+	if begin == len(s) && len(path) == 4 {
+		*res = append(*res, strings.Join(path, "."))
+		return
+	}
+
+	for i := begin; i < len(s) && i < begin+3; i++ {
+		sub := s[begin : i+1]
+		if ok := checkipnum(sub); ok {
+			path = append(path, sub)
+			dfs(s, i+1, path, res)
+			path = path[:len(path)-1]
+		}
+	}
+}
+
+func dfsmain() {
+	var (
+		input = "125610211"
+		res   []string
+		path  = []string{}
+	)
+	dfs(input, 0, path, &res)
+
+	fmt.Printf("dfs  res %v\n", res)
+}
 
 func maket() [][][4]int {
 	t := make([][][4]int, 12-4+1)
@@ -19,15 +57,17 @@ func maket() [][][4]int {
 	return t
 }
 
-func main() {
-	var t = maket()
-
-	var input = "125610211"
-	var ipstr = [4]string{}
-	var i = len(input)
+func list() {
+	var (
+		t     = maket()
+		input = "125610211"
+		ipstr = [4]string{}
+		i     = len(input)
+		res   = []string{}
+	)
 labeltotal:
-	for k, v := range t[i-4] {
-		fmt.Printf("%d-%d,%d,%d,%d----", k, v[0], v[1], v[2], v[3])
+	for _, v := range t[i-4] {
+		// fmt.Printf("%d-%d,%d,%d,%d----", k, v[0], v[1], v[2], v[3])
 		ipstr[0] = input[0:v[0]]
 		ipstr[1] = input[v[0] : v[0]+v[1]]
 		ipstr[2] = input[v[0]+v[1] : v[0]+v[1]+v[2]]
@@ -38,29 +78,28 @@ labeltotal:
 			}
 
 		}
-		fmt.Printf("%v", string(ipstr[0]))
-		for i := 1; i < 4; i++ {
-			fmt.Printf(".%v", string(ipstr[i]))
-		}
 
-		fmt.Printf("\n")
+		// fmt.Printf("%s\n", strings.Join(ipstr[:], "."))
+
+		res = append(res, strings.Join(ipstr[:], "."))
 
 	}
+	fmt.Printf("list res %v\n", res)
 
 }
 
 func checkipnum(input string) bool {
 	if len(input) > 1 && input[0] == '0' {
-		fmt.Printf("CHECK FALSE,0 start,%s\n", string(input))
+		// fmt.Printf("CHECK FALSE,0 start,%s\n", string(input))
 		return false
 	}
 	numip, err := strconv.Atoi(string(input))
 	if err != nil {
-		fmt.Printf("CHECK FALSE,%s\n", err.Error())
+		// fmt.Printf("CHECK FALSE,%s\n", err.Error())
 		return false
 	}
 	if numip > 255 || numip < 0 {
-		fmt.Printf("CHECK FALSE,numip %v\n", numip)
+		// fmt.Printf("CHECK FALSE,numip %v\n", numip)
 		return false
 	}
 
