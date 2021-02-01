ac8e8189576a3a46159d8b0c188bf8b3425dbe91
Wurui Chen <chenwurui@163.com>
2021-02-01 16:18:44 +0800

    make func

--- iprestore.go
+++ iprestore.go
@@ -5,30 +5,33 @@ import (
 	"strconv"
 )
 
-func main() {
-	var t = make([][][4]int, 9)
+func maket() [][][4]int {
+	t := make([][][4]int, 12-4+1)
 	for i := 1; i < 4; i++ {
 		for j := 1; j < 4; j++ {
 			for k := 1; k < 4; k++ {
 				for l := 1; l < 4; l++ {
-					// fmt.Printf("%d.%d.%d.%d--%d\n", i, j, k, l, i+j+k+l)
 					t[i+j+k+l-4] = append(t[i+j+k+l-4], [4]int{i, j, k, l})
 				}
 			}
 		}
 	}
+	return t
+}
+
+func main() {
+	var t = maket()
 
 	var input = "125610211"
-	var bytea = []byte(input)
-	var ipstr = [4][]byte{}
+	var ipstr = [4]string{}
 	var i = len(input)
 labeltotal:
 	for k, v := range t[i-4] {
 		fmt.Printf("%d-%d,%d,%d,%d----", k, v[0], v[1], v[2], v[3])
-		ipstr[0] = bytea[0:v[0]]
-		ipstr[1] = bytea[v[0] : v[0]+v[1]]
-		ipstr[2] = bytea[v[0]+v[1] : v[0]+v[1]+v[2]]
-		ipstr[3] = bytea[v[0]+v[1]+v[2]:]
+		ipstr[0] = input[0:v[0]]
+		ipstr[1] = input[v[0] : v[0]+v[1]]
+		ipstr[2] = input[v[0]+v[1] : v[0]+v[1]+v[2]]
+		ipstr[3] = input[v[0]+v[1]+v[2]:]
 		for i := 0; i < 4; i++ {
 			if !checkipnum(ipstr[i]) {
 				continue labeltotal
@@ -46,7 +49,7 @@ labeltotal:
 
 }
 
-func checkipnum(input []byte) bool {
+func checkipnum(input string) bool {
 	if len(input) > 1 && input[0] == '0' {
 		fmt.Printf("CHECK FALSE,0 start,%s\n", string(input))
 		return false
